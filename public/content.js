501a0bce84b937d20ba64274c59e8e6ef902f616
Eduardo Silveira <eduardo.silveira@vengreso.com>
2023-12-03 02:41:46 -0300

    feat():add export function and input

--- public/content.js
+++ public/content.js
@@ -1,7 +1,7 @@
 /*global chrome*/
 var OPENAI_API_KEY = "";
 let _url = window.location.href;
-if (_url.includes('meet.google') || _url.includes('teams.live')) {
+if (_url.includes('meet.google') || _url.includes('teams.live') || _url.includes('teams.microsoft')) {
   run();
 }
 chrome.runtime.onMessage.addListener((event) => {
@@ -23,6 +23,14 @@ let selectedHistory = [];
 let myActors = new Set();
 let text = [];
 let selectedActor = '';
+let myRequest = ``;
+
+var alertType = {
+  success: 'green',
+  warning: 'yellow',
+  error: 'red'
+}
+
 
 function run() {
   chrome.runtime.sendMessage({ action: 'storage.get' }, (ev) => {
@@ -282,13 +290,6 @@ function run() {
     setTimeout(readText, 3000);
   }
 
-
-  alertType = {
-    success: 'green',
-    warning: 'yellow',
-    error: 'red'
-  }
-
   function updateAlert(text, time, type) {
     let alert = document.getElementsByClassName('alert-msg')[0];
     alert.innerHTML = text;
@@ -307,6 +308,17 @@ function run() {
     alert.classList.add('alert-msg')
     return alert
   }
+
+  function createInput() {
+    let input = document.createElement(`input`)
+    input.type = `text`
+    input.id = `txtInput`
+    input.classList.add(`my-input`)
+    input.addEventListener(`keyup`, ev => myRequest = ev.target.value)
+    input.placeholder = `Additional details (e.g., Language, Key Topics, Context, Tone, Audience)`
+    return input
+  }
+
   function getView() {
     let myView = document.getElementById('myView');
     if (!myView) {
@@ -322,10 +334,12 @@ function run() {
       let header = document.createElement(`div`)
       header.classList.add(`my-header`)
       header.append(actors())
+      header.append(exporter())
       header.append(collapseView(myMainView))
       myMainView.append(header)
       myMainView.append(myView)
       myMainView.append(createAlert())
+      myMainView.append(createInput())
       myButtonsView.append(createRequester())
       myButtonsView.append(createClear())
       myMainView.append(myButtonsView)
@@ -394,10 +408,10 @@ function run() {
     let myBtns = document.createElement('div');
     myBtns.classList.add('my-btns');
     [
-      { text: 'Get Insights', id: 'insightsBtn', prompt: 'Tell me more about. Show it like a website, easy and short and simple and clear.' },
-      { text: 'Explore More', id: 'explainBtn', prompt: 'Whats cool about? Show it like a website, easy and short and simple and clear.' },
-      { text: 'Simply Explain', id: 'simpleBtn', prompt: "Explain easily. Show it like a website, easy and short and simple and clear." },
-      { text: 'Tips & Tricks', id: 'tipsBtn', prompt: "Gimme some tips and tricks about. Show it like a website, easy and short and simple and clear." },
+      { text: 'Get Insights', id: 'insightsBtn', prompt: 'Tell more about.' },
+      { text: 'Explore More', id: 'explainBtn', prompt: 'What is cool about?' },
+      { text: 'Simply Explain', id: 'simpleBtn', prompt: "Explain me easily." },
+      { text: 'Tips & Tricks', id: 'tipsBtn', prompt: "Gimme some tips and tricks." },
     ].map(btn => {
       let myBtn = document.createElement('div');
       myBtn.id = btn.id
@@ -407,6 +421,50 @@ function run() {
       myBtns.append(myBtn)
     })
     return myBtns
+  }
+  function exportToText() {
+    // Create a Blob with the content
+    var blob = new Blob([printHistory.map((i) => { return `${i.key} (${formatTime(i.time)}): ${i.value}` }).join(`,\n`)], { type: 'text/plain' });
+    // Create a link element
+    var link = document.createElement('a');
+
+    // Set the link's attributes
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'exported.txt';
+
+    // Append the link to the document
+    document.body.appendChild(link);
+
+    // Trigger a click on the link to initiate the download
+    link.click();
+
+    // Remove the link from the document
+    document.body.removeChild(link);
+  }
+
+  function exporter() {
+    let myBtn = document.createElement('div');
+    myBtn.id = 'myExportBtn';
+    myBtn.innerHTML = `<svg fill="#000000" height="28px" width="51px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+      viewBox="0 0 493.525 493.525" xml:space="preserve">
+      <g id="XMLID_30_">
+      <path id="XMLID_32_" d="M430.557,79.556H218.44c21.622,12.688,40.255,29.729,54.859,49.906h157.258
+       c7.196,0,13.063,5.863,13.063,13.06v238.662c0,7.199-5.866,13.064-13.063,13.064H191.894c-7.198,0-13.062-5.865-13.062-13.064
+       V222.173c-6.027-3.1-12.33-5.715-18.845-7.732c-3.818,11.764-12.105,21.787-23.508,27.781c-2.39,1.252-4.987,2.014-7.554,2.844
+       v136.119c0,34.717,28.25,62.971,62.968,62.971h238.663c34.718,0,62.969-28.254,62.969-62.971V142.522
+       C493.525,107.806,465.275,79.556,430.557,79.556z"/>
+      <path id="XMLID_31_" d="M129.037,175.989c51.419,1.234,96.388,28.283,122.25,68.865c2.371,3.705,6.434,5.848,10.657,5.848
+       c1.152,0,2.322-0.162,3.46-0.486c5.377-1.545,9.114-6.418,9.179-12.006c0-0.504,0-1.01,0-1.51
+       c0-81.148-64.853-147.023-145.527-148.957V64.155c0-5.492-3.038-10.512-7.879-13.078c-2.16-1.139-4.533-1.707-6.889-1.707
+       c-2.94,0-5.848,0.88-8.35,2.584L5.751,120.526C2.162,122.98,0.018,127.041,0,131.394c-0.017,4.338,2.113,8.418,5.687,10.902
+       l100.17,69.451c2.518,1.753,5.459,2.631,8.414,2.631c2.355,0,4.696-0.553,6.857-1.676c4.855-2.549,7.909-7.6,7.909-13.092V175.989z
+       "/>
+      </g>
+      </svg>`;
+    myBtn.onclick = () => {
+      exportToText()
+    }
+    return myBtn
   }
 
   function createClear() {
@@ -437,15 +495,42 @@ function run() {
   }
 
   getView().innerHTML = ''
+  function sendMessage(prompt, message = '', inital = false) {
+    let historyMessage = printHistory.filter(item => item.selected).map((item) => item.value).join('');
+    if (historyMessage && historyMessage.length != 0) message = historyMessage
 
-  function sendMessage(prompt) {
-    let message = printHistory.filter(item => item.selected).map((item) => item.value).join('');
-
-    if (!message || message.length == '') {
+    if (message.length == 0 && myRequest == '') {
       updateAlert('Select a message!', 15, alertType.warning)
       return
     }
+    let messages = [
+      {
+        "role": "system",
+        "content": `${prompt}. Your response have to be clean and concise, 
+        as a Website HTML to be injected on innerHtml,
+        divide by sections, and topics, with HTML tags.`
+      }
+    ];
 
+    if (message.length > 0) {
+      messages.push(
+        {
+          "role": "user",
+          "content":
+            `${message}`
+        }
+      )
+    }
+
+    if (myRequest) {
+      messages.push(
+        {
+          "role": "user",
+          "content":
+            `${myRequest}`
+        }
+      )
+    }
     updateAlert('Requesting...!', null, alertType.success)
     fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
@@ -455,17 +540,7 @@ function run() {
       },
       body: JSON.stringify({
         model: 'gpt-3.5-turbo',
-        messages: [
-          {
-            "role": "system",
-            "content": `Return as Website in HTML with sections spliting in topics, following: ${prompt}`
-          },
-          {
-            "role": "user",
-            "content":
-              `About: ${message}`
-          }
-        ]
+        messages: messages
       })
     }).then((response) => response.json())
       .then((data) => {
